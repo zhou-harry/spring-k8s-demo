6f33819591d33868936d08b3e470c5b83a291558
zhouhong <admin@example.com>
2021-03-24 19:00:19 +0800

    commit

---
+++ spring-client/src/main/java/com/harry/springk8sclient/feign/falback/ClientFallback.java
@@ -0,0 +1,12 @@
+package com.harry.springk8sclient.feign.falback;
+
+import com.harry.springfeignapi.fallback.ServerFallback;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientFallback extends ServerFallback {
+  @Override
+  public String getServer() {
+    return "服务请求失败了============";
+  }
+}
