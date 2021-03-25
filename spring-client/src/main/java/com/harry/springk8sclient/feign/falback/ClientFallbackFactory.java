36327794d385c89ee003d567a0875d1226483260
zhouhong <admin@example.com>
2021-03-25 11:30:54 +0800

    commit

--- spring-client/src/main/java/com/harry/springk8sclient/feign/falback/ClientFallbackFactory.java
+++ spring-client/src/main/java/com/harry/springk8sclient/feign/falback/ClientFallbackFactory.java
@@ -1,9 +1,9 @@
 package com.harry.springk8sclient.feign.falback;
 
 import com.harry.springk8sclient.feign.FeignClientApiImpl;
+import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
 
 @Slf4j
