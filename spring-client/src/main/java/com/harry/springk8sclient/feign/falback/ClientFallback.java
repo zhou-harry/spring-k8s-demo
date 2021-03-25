f41c2e996cf279af8fe8dbe381385623991b9622
zhouhong <admin@example.com>
2021-03-25 11:17:39 +0800

    commit

--- spring-client/src/main/java/com/harry/springk8sclient/feign/falback/ClientFallback.java
+++ spring-client/src/main/java/com/harry/springk8sclient/feign/falback/ClientFallback.java
@@ -1,10 +1,10 @@
 package com.harry.springk8sclient.feign.falback;
 
-import com.harry.springfeignapi.feign.FeignClientApi;
+import com.harry.springk8sclient.feign.FeignClientApiImpl;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ClientFallback implements FeignClientApi {
+public class ClientFallback implements FeignClientApiImpl {
   @Override
   public String getServer() {
     return "服务请求失败了============";
