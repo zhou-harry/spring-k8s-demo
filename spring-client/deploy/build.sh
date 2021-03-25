dccf0ab2607d9b28d16aa0372fbcaf2ccc40d305
zhouhong <admin@example.com>
2021-03-25 10:49:49 +0800

    commit

--- spring-client/deploy/build.sh
+++ spring-client/deploy/build.sh
@@ -4,13 +4,15 @@ APP_NAME=$1
 API_VERSION="0.0.1-SNAPSHOT"
 TAG=dev-$(date +%m%d%H%M)-${BUILD_NUMBER}
 #如果下面需要将镜像推送到镜像仓库，那么此处的镜像名称需要带上镜像仓库地址
-IMAGE_NAME="k8s-master:8015/caohua_rep/${APP_NAME}:$TAG"
+#IMAGE_NAME="k8s-master:8015/caohua_rep/${APP_NAME}:$TAG"
+IMAGE_NAME="caohua76:8015/caohua_rep/${APP_NAME}:$TAG"
 #进入target目录复制Dockerfile文件
 cd ${WORKSPACE}/${APP_NAME}/target/deploy
 #构建docker镜像
 docker build -t $IMAGE_NAME .
 #登录Harbor镜像仓库（此处的仓库地址需要与上面IMAGE_NAME的仓库地址保持一致否则推送denied）
-docker login -u caohua -p Qwer1234 k8s-master:8015
+#docker login -u caohua -p Qwer1234 k8s-master:8015
+docker login -u caohua -p Qwer1234 caohua76:8015
 #推送docker镜像到Harbor镜像仓库
 docker push $IMAGE_NAME
 #删除本机镜像
