7b0703bba18f310ab17edbe398fbaeffb9b24725
zhouhong <admin@example.com>
2021-03-25 14:48:19 +0800

    commit

--- spring-client/src/main/java/com/harry/springk8sclient/controller/ClientController.java
+++ spring-client/src/main/java/com/harry/springk8sclient/controller/ClientController.java
@@ -1,30 +1,36 @@
 package com.harry.springk8sclient.controller;
 
 import com.harry.springk8sclient.feign.FeignClientApiImpl;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import java.util.List;
 import java.util.Random;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class ClientController {
+
   private final static Random random = new Random();
   @Autowired
   private FeignClientApiImpl feignClientApi;
+
   /**
    * Feign方式调用依赖服务
    *
    * @return
    */
-//  @HystrixCommand(
-//      commandProperties = { // Command 熔断配置
-//          // 设置操作时间为 100 毫秒
-//          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
-//      },
-//      fallbackMethod = "getServerFallback" // 设置 fallback 方法
-//  )
+  @HystrixCommand(
+      commandProperties = { // Command 熔断配置
+          // 设置操作时间为 100 毫秒
+          @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "100")
+      },
+      fallbackMethod = "getServerFallback" // 设置 fallback 方法
+  )
   @GetMapping("/server")
   public String getServer() throws InterruptedException {
     long executeTime = random.nextInt(200);
@@ -34,7 +40,18 @@ public class ClientController {
 
     return feignClientApi.getServer();
   }
-  public String getServerFallback(){
+
+  public String getServerFallback() {
     return "执行时间过长，触发熔断......";
+  }
+
+  @GetMapping("/service")
+  public List<String> getServiceList() {
+    return feignClientApi.getServiceList();
+  }
+
+  @GetMapping("/instance")
+  public Object getInstance(@RequestParam("name") String name) {
+    return feignClientApi.getInstance(name);
   }
 }
