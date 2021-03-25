88f4b6f05eea711907ab06042b0b946da55510d6
zhouhong <admin@example.com>
2021-03-25 10:12:56 +0800

    commit

--- spring-client/src/main/java/com/harry/springk8sclient/feign/FeignClientApiImpl.java
+++ spring-client/src/main/java/com/harry/springk8sclient/feign/FeignClientApiImpl.java
@@ -3,7 +3,11 @@ package com.harry.springk8sclient.feign;
 import com.harry.springk8sclient.feign.falback.ClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "${provider.service.name}", fallback = ClientFallback.class)
+@FeignClient(
+    value = "${provider.service.name}",
+    url = "${provider.service.name}",
+    fallback = ClientFallback.class
+)
 public interface FeignClientApiImpl {
 
 }
