b27b0543d21c2ae72db35834f04339dc431c876c
zhouhong <admin@example.com>
2021-03-25 15:29:56 +0800

    commit

--- spring-feign-api/src/main/java/com/harry/springfeignapi/domain/ServiceDomain.java
+++ spring-feign-api/src/main/java/com/harry/springfeignapi/domain/ServiceDomain.java
@@ -1,12 +1,15 @@
 package com.harry.springfeignapi.domain;
 
 import java.io.Serializable;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ServiceDomain implements Serializable {
-
   private String name;
 }
