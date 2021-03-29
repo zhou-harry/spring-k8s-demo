614d3350df3e8085cbaca4caba0d7cac830a8a1e
zhouhong <admin@example.com>
2021-03-29 14:40:04 +0800

    commit-swagger

--- spring-feign-api/src/main/java/com/harry/springfeignapi/feign/FeignClientApi.java
+++ spring-feign-api/src/main/java/com/harry/springfeignapi/feign/FeignClientApi.java
@@ -4,7 +4,9 @@ import com.harry.springfeignapi.domain.ServiceDomain;
 import com.harry.springfeignapi.feign.falback.ClientFallbackFactory;
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,18 +17,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 )
 public interface FeignClientApi {
 
-
+  @GetMapping("/server")
   String getServer();
 
-
+  @GetMapping("/service")
   List<String> getServiceList();
 
-
+  @GetMapping("/instance")
   Object getInstance(@RequestParam("name") String name);
 
-
+  @GetMapping("/instance2/{name}")
   Object getInstanceByName(@PathVariable("name") String name);
 
-
+  @PostMapping("/instance3")
   Object postInstance(@RequestBody ServiceDomain domain);
 }
--- spring-server/src/main/java/com/harry/springk8sserver/controller/BaseController.java
+++ spring-server/src/main/java/com/harry/springk8sserver/controller/BaseController.java
@@ -9,8 +9,6 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -26,35 +24,35 @@ public class BaseController implements FeignClientApi {
 
   @Override
   @ApiOperation(value = "获取服务提供者信息")
-  @GetMapping("/server")
+
   public String getServer() {
     return providerService.getServer();
   }
 
   @Override
   @ApiOperation(value = "获取服务列表信息")
-  @GetMapping("/service")
+
   public List<String> getServiceList() {
     return discoveryClient.getServices();
   }
 
   @Override
   @ApiOperation(value = "根据参数名称获取服务信息")
-  @GetMapping("/instance")
+
   public Object getInstance(String name) {
     return discoveryClient.getInstances(name);
   }
 
   @Override
   @ApiOperation(value = "根据变量名称获取服务信息")
-  @GetMapping("/instance2/{name}")
+
   public Object getInstanceByName(String name) {
     return discoveryClient.getInstances(name);
   }
 
   @Override
   @ApiOperation(value = "根据参数对象获取服务信息")
-  @PostMapping("/instance3")
+
   public Object postInstance(ServiceDomain domain) {
     return discoveryClient.getInstances(domain.getName());
   }
