d482de2ba93ff71f124e40f68881e052c8d78bac
zhouhong <admin@example.com>
2021-04-13 18:42:31 +0800

    commit

--- center/refresh.sh
+++ center/refresh.sh
@@ -3,5 +3,6 @@
 APP_NAME=$1
 PROFILE=$2
 cd ${WORKSPACE}/center
+kubectl apply -f app-crb.yaml
 kubectl apply -f ./config/${APP_NAME}-${PROFILE}.yaml
 kubectl apply -f ./secret/${APP_NAME}-secret.yaml
