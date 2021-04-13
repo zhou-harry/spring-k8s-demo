5e98b70d893d4708ccfced8a9c37b85a157d1ca5
zhouhong <admin@example.com>
2021-04-13 16:28:56 +0800

    commit

--- spring-server/deploy/deploy.sh
+++ spring-server/deploy/deploy.sh
@@ -3,7 +3,7 @@
 APP_NAME=$1
 PROFILE=$2
 cd ${WORKSPACE}/config
-kubectl apply configmap spring-k8s-server-config --from-file=${APP_NAME}-${PROFILE}.yaml
+kubectl apply configmap ${APP_NAME}-config --from-file=${APP_NAME}-${PROFILE}.yaml
 cd ${WORKSPACE}/config/secret
 kubectl apply configmap ${APP_NAME}-secret --from-file=${APP_NAME}-secret.yaml
 cd ${WORKSPACE}/${APP_NAME}/target/deploy
