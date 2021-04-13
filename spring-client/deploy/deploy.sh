ec5cf11686732afbea835daba8063664e79113fb
zhouhong <admin@example.com>
2021-04-13 16:24:01 +0800

    commit

--- spring-client/deploy/deploy.sh
+++ spring-client/deploy/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 APP_NAME=$1
+PROFILE=$2
+cd ${WORKSPACE}/config
+kubectl apply configmap ${APP_NAME}-config --from-file=${APP_NAME}-${PROFILE}.yaml
+cd ${WORKSPACE}/config/secret
+kubectl apply configmap ${APP_NAME}-secret --from-file=${APP_NAME}-secret.yaml
 cd $WORKSPACE/$APP_NAME/target/deploy
 kubectl apply -f deploy.yaml
--- spring-server/deploy/deploy.sh
+++ spring-server/deploy/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 APP_NAME=$1
-cd $WORKSPACE/$APP_NAME/target/deploy
+PROFILE=$2
+cd ${WORKSPACE}/config
+kubectl apply configmap spring-k8s-server-config --from-file=${APP_NAME}-${PROFILE}.yaml
+cd ${WORKSPACE}/config/secret
+kubectl apply configmap ${APP_NAME}-secret --from-file=${APP_NAME}-secret.yaml
+cd ${WORKSPACE}/${APP_NAME}/target/deploy
 kubectl apply -f deploy.yaml
