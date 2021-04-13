e942d1b7cd7456a488a176b2aa95969bbdf20342
zhouhong <admin@example.com>
2021-04-13 16:41:23 +0800

    commit

--- spring-server/src/main/java/com/harry/springk8sserver/ServerApp.java
+++ spring-server/src/main/java/com/harry/springk8sserver/ServerApp.java
@@ -1,6 +1,5 @@
 package com.harry.springk8sserver;
 
-import com.harry.springcommon.swagger.EnableSwagger;
 import com.harry.springk8sserver.config.CoreProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
-@EnableSwagger
 @EnableConfigurationProperties(CoreProperties.class)
 public class ServerApp {
 
